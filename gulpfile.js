fd842ba8d43bf820968f8a6a7e38332c8ebbe280
KateNegrienko <katya@negrienko.com>
2017-03-01 14:34:52 +0200

    Chage gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -33,14 +33,29 @@ var sass = require('gulp-sass');
 
 gulp.task('build-scss', function () {
     
-  return gulp.src(['./src/colors/*.scss'])
+  return gulp.src(['./src/colors/colors_var.scss',
+            './src/colors/colors_mixins.scss',
+            './src/colors/colors_bootbarn.scss',
+            './src/colors/colors_default.scss',
+            './src/flexbox/flexbox_mixins.scss',
+            './src/positions/positions_var.scss',
+            './src/positions/positions_mixins.scss',
+            './src/sizes/sizes_mixins.scss',
+            './src/typography/typography_var.scss',
+            './src/typography/typography_mixins.scss',
+            './src/effects/animation_mixins.scss',
+            './src/effects/transition_mixins.scss',
+            './src/effects/transform_mixins.scss',
+            './src/effects/effects_var.scss',
+            './src/effects/effects_mixins.scss',
+            './src/controls/controls_mixins.scss'])
     .pipe(concat(pkg.name + '.scss'))
     .pipe(gulp.dest('./dist'));
 });
 
 // Define build tasks      
 gulp.task('build-sass', ['build-scss', 'build-sass-dev', 'build-sass-prod']);  
-gulp.task('build', ['build-dev', 'build-prod', 'build-less']);
+gulp.task('build', ['build-dev', 'build-prod']); // 'build-less'
 gulp.task('rebuild', ['build-dev']);
 gulp.task('clean', ['build-clean']);
 gulp.task('watch', ['build-watch']);
