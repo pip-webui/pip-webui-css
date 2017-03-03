4471400ff00920280bcf484eb25383d6bc359f71
KateNegrienko <katya@negrienko.com>
2017-03-03 21:54:16 +0200

    Delelte all less files

--- gulpfile.js
+++ gulpfile.js
@@ -6,29 +6,6 @@ var pkg = require('./package');
 // Add standard tasks    
 require('pip-webui-tasks').all(gulp);
 
-gulp.task('build-less', function() {
-    return gulp.src([
-            './src/colors/colors_var.less',
-            './src/colors/colors_mixins.less',
-            './src/colors/colors_bootbarn.less',
-            './src/colors/colors_default.less',
-            './src/flexbox/flexbox_mixins.less',
-            './src/positions/positions_var.less',
-            './src/positions/positions_mixins.less',
-            './src/sizes/sizes_mixins.less',
-            './src/typography/typography_var.less',
-            './src/typography/typography_mixins.less',
-            './src/effects/animation_mixins.less',
-            './src/effects/transition_mixins.less',
-            './src/effects/transform_mixins.less',
-            './src/effects/effects_var.less',
-            './src/effects/effects_mixins.less',
-            './src/controls/controls_mixins.less',
-        ])
-        .pipe(concat(pkg.name + '.less'))
-        .pipe(gulp.dest('./dist'));
-});
-
 var sass = require('gulp-sass');
 
 gulp.task('build-scss', function () {
@@ -55,7 +32,7 @@ gulp.task('build-scss', function () {
 
 // Define build tasks      
 gulp.task('build-sass', ['build-scss', 'build-sass-dev', 'build-sass-prod']);  
-gulp.task('build', ['build-dev', 'build-prod', 'build-less', 'build-sass']); // 'build-less'
+gulp.task('build', ['build-dev', 'build-prod', 'build-sass']); // 'build-less'
 gulp.task('rebuild', ['build-dev']);
 gulp.task('clean', ['build-clean']);
 gulp.task('watch', ['build-watch']);
