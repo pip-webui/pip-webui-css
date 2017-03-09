0493079a4850436f777a80170b00c7f70686b8de
Sergey Seroukhov <seroukhov@gmail.com>
2017-03-08 19:34:38 -0700

    'Full rebuilt'

--- gulpfile.js
+++ gulpfile.js
@@ -32,8 +32,8 @@ gulp.task('build-scss', function () {
 
 // Define build tasks      
 gulp.task('build-sass', ['build-scss', 'build-sass-dev', 'build-sass-prod']);  
-gulp.task('build', ['build-dev', 'build-prod', 'build-sass']); // 'build-less'
-gulp.task('rebuild', ['build-dev']);
+gulp.task('rebuild', ['build-dev', 'build-prod', 'build-sass']); // 'build-less'
+gulp.task('build', ['build-dev']);
 gulp.task('clean', ['build-clean']);
 gulp.task('watch', ['build-watch']);
 gulp.task('jshint', ['test-jshint']);
