756c011b43402025ae763eda01f59b32ca65b548
KateNegrienko <katya@negrienko.com>
2017-03-30 18:06:43 +0300

    fix problems with pip-webui-css.scss

--- gulpfile.js
+++ gulpfile.js
@@ -8,32 +8,29 @@ require('pip-webui-tasks').all(gulp);
 
 var sass = require('gulp-sass');
 
-gulp.task('build-scss', function () {
+gulp.task('build-scss-css', function () {
 
-  return gulp.src(['./src/colors/colors_var.scss',
-    './src/colors/colors_mixins.scss',
-    './src/colors/colors_bootbarn.scss',
-    './src/colors/colors_default.scss',
-    './src/flexbox/flexbox_mixins.scss',
-    './src/positions/positions_var.scss',
-    './src/positions/positions_mixins.scss',
-    './src/sizes/sizes_mixins.scss',
-    './src/typography/typography_var.scss',
-    './src/typography/typography_mixins.scss',
-    './src/effects/animation_mixins.scss',
-    './src/effects/transition_mixins.scss',
-    './src/effects/transform_mixins.scss',
-    './src/effects/effects_var.scss',
-    './src/effects/effects_mixins.scss',
-    './src/controls/controls_mixins.scss'])
+  return gulp.src(['./src/colors/ColorVariables.scss',
+    './src/controls/ControlsMixins.scss',
+    './src/effects/AnimationMixins.scss',
+    './src/effects/EffectMixins.scss',
+    './src/effects/TransformMixins.scss',
+    './src/effects/EffectsVariables.scss',
+    './src/effects/TransitionMixins.scss',
+    './src/flexbox/FlexboxMixins.scss',
+    './src/positions/PositionsMixins.scss',
+    './src/positions/PositionsVariables.scss',
+    './src/sizes/SizeMixins.scss',
+    './src/typography/TypographyVariables.scss',
+    './src/typography/TypographyMixins.scss'])
     .pipe(concat(pkg.name + '.scss'))
     .pipe(gulp.dest('./dist'));
 });
 
 // Define build tasks      
-gulp.task('build-sass', ['build-scss', 'build-sass-dev', 'build-sass-prod']);
+gulp.task('build-sass', ['build-scss-css', 'build-sass-dev', 'build-sass-prod']);
 gulp.task('rebuild', ['build-dev', 'build-prod', 'build-sass']); // 'build-less'
-gulp.task('build', ['build-dev']);
+gulp.task('build', ['build-dev', 'build-scss']);
 gulp.task('clean', ['build-clean']);
 gulp.task('watch', ['build-watch']);
 gulp.task('jshint', ['test-jshint']);
